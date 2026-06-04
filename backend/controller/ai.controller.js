145c863e52809e99fa1b4d0141ebdc12aac00fdb
ojas chauhan <ojaschauhan587@gmail.com>
2026-06-04 11:00:18 +0530

    done

--- backend/controller/ai.controller.js
+++ backend/controller/ai.controller.js
@@ -1,7 +1,7 @@
 import generateResponse from "../src/services/ai.service.js";
 import Review from "../models/review.model.js"; 
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 // --- 1. POST ROUTE: Generate Review & Save to History ---
